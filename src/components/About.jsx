0c1c8aca6d539746202a05f8fdc5203c3043daac
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-12-13 21:29:57 +0530

    class components create

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,5 @@ const About = ()=>{
        <UserClass/>
        </div>
     )
-
 }
 export default About
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -8,7 +8,6 @@ function user(props) {
       
     </div>
   )
-  
 }
 export default user
 
