d4bb5d6f42d93a230cf560844a0dd3313dbd27f4
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-12-14 20:24:37 +0530

    class components

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -4,9 +4,9 @@ function user(props) {
     <div className="User">
         <h3>Name : {props.name} </h3>
         <h4>Age :</h4>
-        <h5>stark</h5>
-      
+        <h5>stark</h5>   
     </div>
   )
 }
+
 export default user
