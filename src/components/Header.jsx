5f42c4bcc73b2245dc75208ecc2927278b80dde4
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-12-16 23:47:37 +0530

    Header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,6 +31,9 @@ const Header = () => {
             }else if(loignReact=== "logout"){
               SetloginReact("login")
             }
+
+
+            
           
           }}>{loignReact}</button>
         </ul>
