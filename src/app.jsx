74ed2bc68bde5cf1c2f3e00fa1a26ee39bc85c92
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2024-12-17 20:16:24 +0530

    App change

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Link, Route, Router } from "wouter"; // Importing Wouter
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
