f01c23ddf35f9fb1c040d5d23733055aa1fe51e0
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-01-03 22:54:49 +0530

    update userclass

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,8 +6,7 @@
             <div className="User">
                 <h3>Name : </h3>
                 <h4>Age :</h4>
-                <h5>stark</h5>
-              
+                <h5>stark</h5>  
             </div>
           )
     }
